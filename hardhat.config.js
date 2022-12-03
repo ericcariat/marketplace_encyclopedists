b7794df725fe959a3ce2bc0ddf6ace93cb84c0da
Eric Cariat <eric_cariat@yahoo.com>
2022-12-03 23:49:45 +0100

    [EP-6] update hardhat config & deply script

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     goerli: {
       url: process.env.REACT_APP_ALCHEMY_API_URL,
-      accounts: [ process.env.REACT_APP_PRIVATE_KEY ]
+      account: [ `${process.env.REACT_APP_PRIVATE_KEY}` ],
     }
   },
   solidity: {
